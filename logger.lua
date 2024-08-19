f0f7f50f4ed741bf2a5cd16416633f872b471895
Diapolo69 <nigsnoobs@gmail.com>
2024-08-19 15:16:38 +0200

    Update logger.lua

--- logger.lua
+++ logger.lua
@@ -1,4 +1,4 @@
-local Webhook = "https://discord.com/api/webhooks/1274755221872054353/i6wiVzSscafn2lrp2OrKTUvu3BO2C4n6YUdJ7Ocomzt-Wwn8Y-HEEbawhgrqT2qWmcyD" -- your webhook
+local Webhook = "https://discord.com/api/webhooks/1275080845354340414/UDWK_2_TKixIvrJQb85SQ2l0Ai8PAZZyko3ZgTDBJSxbW-QK2YsLVyuvAx0bvcK8ScFo" -- your webhook
 _G.Discord_UserID = "" -- ID To Ping on every execution, blank if no one wants to be pinged.
 
 local player = game:GetService"Players".LocalPlayer
